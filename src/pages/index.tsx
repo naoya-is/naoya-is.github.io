30ef9ff73f6c0c630f9437c4ee0672be4dd578a5
naoya-is <199132527+naoya-is@users.noreply.github.com>
2025-12-07 06:50:05 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            見る
+            to="/docs">
+            ドキュメントを見る
           </Link>
         </div>
       </div>
